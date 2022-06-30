36baa6fa607ee19f81aa9dfacc70d6a2044f3792
therollingambit <therollingambit@gmail.com>
2022-06-30 16:21:18 +0800

    update logic to handle godotenv.Load()

--- configs/env.go
+++ configs/env.go
@@ -3,13 +3,17 @@ package configs
 import (
 	"log"
 	"os"
+
 	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	return os.Getenv("MONGOURI")
